0a444be94bff662c7c2c7f3a09d0f3c28e9005d9
satyamjha <satyamjhaactor07@gmail.com>
2025-02-28 14:30:24 +0530

    again Done

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 
 const MarkdownEditor = () => {
-  const [markdown, setMarkdown] = useState("# Welcome to Markdown Editor\n\nType here...");
+    const [markdown, setMarkdown] = useState("# Heading\n\nType here...");
+
 
   return (
     <div className="app">
